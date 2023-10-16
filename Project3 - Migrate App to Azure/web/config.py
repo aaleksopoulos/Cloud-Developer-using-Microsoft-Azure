6501c0032ccf14b28e1c475189d50eeec1adf84f
Athanasios Alexopoulos <58886889+aaleksopoulos@users.noreply.github.com>
2023-10-16 11:56:07 +0300

    Update config.py

--- Project3 - Migrate App to Azure/web/config.py
+++ Project3 - Migrate App to Azure/web/config.py
@@ -11,13 +11,13 @@ class BaseConfig:
     DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
-    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SECRET_KEY = ''
     SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://project3-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=sauBh31Y0vTt+Cdi+OOXM4u3AD4rDQOuoFhay0KP9bY=' #TODO: Update value
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     # ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     # SENDGRID_API_KEY = '' #Configuration not required, required SendGrid Account
-    ADMIN_EMAIL_ADDRESS= 'athanasios_al@hotmail.com'
-    SENDGRID_API_KEY = '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' 
+    ADMIN_EMAIL_ADDRESS= ''
+    SENDGRID_API_KEY = ''
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
